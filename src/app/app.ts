1a72100aa60791c62c33d08263a147f08283c934
janafathallah <janaahmed1342005@yahoo.com>
2026-06-02 14:58:46 +0300

    update commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,35 +9,35 @@ import { Component, EventEmitter, Input, input, signal } from '@angular/core';
 export class App {
   protected readonly title = signal('PersonalWebsite');
   selectedComponent = '';
-  contact=false;
+  contact = false;
   isDarkMode = false;
 
   onComponent(name: string) {
-      this.selectedComponent = name;
-      const section = document.getElementById(name);
-      section?.scrollIntoView({ behavior: "smooth", block: "start" });
+    this.selectedComponent = name;
+    const section = document.getElementById(name);
+    section?.scrollIntoView({ behavior: "smooth", block: "start" });
   }
 
   onContact(open: boolean) {
-    if(open){
-      this.contact=true;
+    if (open) {
+      this.contact = true;
 
     }
   }
-  onChat(close: boolean){
-    if(close){
-      this.contact=false;
+  onChat(close: boolean) {
+    if (close) {
+      this.contact = false;
     }
   }
 
-  onMode(mode: boolean){
-    if(mode){
+  onMode(mode: boolean) {
+    if (mode) {
       document.body.classList.add('dark-mode');
-      this.isDarkMode=true;
-      
-  } else {
-    document.body.classList.remove('dark-mode');
-      this.isDarkMode=false;
+      this.isDarkMode = true;
+
+    } else {
+      document.body.classList.remove('dark-mode');
+      this.isDarkMode = false;
     }
   }
 }
